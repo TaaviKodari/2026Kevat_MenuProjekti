6107459e5601f259660c8a6fb442fb704cc19eb8
vermin-core <vermin7@proton.me>
2026-02-18 19:31:36 +0200

    fade controlle tehty

---
+++ 2026Kevat_MainMenu/Assets/Scripts/StartLevelController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class StartLevelController : MonoBehaviour
+{
+
+    public FadeController fade;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        fade.FadeOut();    
+    }
+
+}
