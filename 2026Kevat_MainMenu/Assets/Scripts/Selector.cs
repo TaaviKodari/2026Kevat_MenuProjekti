e9189298af960578400b8adac38112022e50767d
vermin-core <vermin7@proton.me>
2026-03-18 19:32:10 +0200

    selector melkein valmis

--- 2026Kevat_MainMenu/Assets/Scripts/Selector.cs
+++ 2026Kevat_MainMenu/Assets/Scripts/Selector.cs
@@ -4,13 +4,14 @@ using TMPro;
 public class Selector : MonoBehaviour
 {
     public TMP_Text [] items;
-    
+    public Color normalColor = Color.white;
+    public Color highlightColor = Color.yellow;
     private int index = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        UpdateColors();
     }
 
     // Update is called once per frame
@@ -20,20 +21,36 @@ public class Selector : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             index = (index - 1 + items.Length) % items.Length;
-            Debug.Log(index);
+            //Debug.Log(index);
+            UpdateColors();
         }
 
         //Alaspäin
         if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             index = (index +1) % items.Length;
-            Debug.Log(index);
+            //Debug.Log(index);
+            UpdateColors();
         }
 
         //Valinta
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            Debug.Log("Indexi on: " + index);
+           ActivateItem(index);
         }
     }
+
+    private void UpdateColors()
+    {
+        for(int i = 0; i < items.Length; i++)
+        {
+            items[i].color = (i == index) ? highlightColor : normalColor;
+        }
+    }
+
+    public void ActivateItem(int i)
+    {
+        Debug.Log("Selected: " + items[i].text);
+    }
+
 }
